80797e9d33eac8dd19a4b854df3da108c4e2c2bc
zzak <zzakscott@gmail.com>
2023-08-19 09:55:03 +0900

    fix

--- lib/buildkite_config.rb
+++ lib/buildkite_config.rb
@@ -1,6 +1,6 @@
 module Buildkite
   module Config
-    autoload :Diff, "./buildkite_config/diff"
-    autoload :PullRequest, "./buildkite_config/pull_request"
+    autoload :Diff, File.expand_path("buildkite_config/diff", __dir__)
+    autoload :PullRequest, File.expand_path("buildkite_config/pull_request", __dir__)
   end
 end
