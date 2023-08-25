e9d2d493aa4a8009df4fb0739c69ea76eb0d2fb9
zzak <zzakscott@gmail.com>
2023-08-25 12:51:03 +0900

    autoload

--- lib/buildkite_config.rb
+++ lib/buildkite_config.rb
@@ -1,5 +1,6 @@
 module Buildkite
   module Config
+    autoload :Annotate, File.expand_path("buildkite_config/annotate", __dir__)
     autoload :Diff, File.expand_path("buildkite_config/diff", __dir__)
     autoload :PullRequest, File.expand_path("buildkite_config/pull_request", __dir__)
     autoload :Repository, File.expand_path("buildkite_config/repository", __dir__)
